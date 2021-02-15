706a64e88547780777a2977c0be7c67fb4759498
Patric Poba <poba.dev@outlook.com>
2021-02-15 16:43:06 +0100

    exclude admin panel from google analytics

--- resources/views/http-requests/index.blade.php
+++ resources/views/http-requests/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app', ['analytics'=> false])
 
 @section('content')
 <div class="container">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
    
-    @if( config('app.env') == 'production')
+    @if( ($analytics ?? true) == true && config('app.env') == 'production')
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168988694-1"></script>
         <script>
