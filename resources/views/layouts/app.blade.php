8371afd023b22f477d2271a2b842b7e99bd8233e
Patric Poba <poba2010@live.com>
2021-03-11 07:05:35 +0000

    Remove google analytics code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,13 +21,7 @@
    
     @if( ($analytics ?? true) == true && config('app.env') == 'production')
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168988694-1"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-168988694-1');
-        </script> 
+         
     @endif
 </head>
 <body>
