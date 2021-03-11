7f4d9a5b1ba8bae5a4c67fc6d74db59435549e44
Patric Poba <poba2010@live.com>
2021-03-11 07:18:35 +0000

    Add G4A google analytics tracking code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,9 +19,16 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
    
-    @if( ($analytics ?? true) == true && config('app.env') == 'production')
+    @if( ($analytics ?? true) == true && config('app.env') == 'production') 
         <!-- Global site tag (gtag.js) - Google Analytics -->
-         
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-K2V0QLNH5F"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-K2V0QLNH5F');
+        </script>
     @endif
 </head>
 <body>
