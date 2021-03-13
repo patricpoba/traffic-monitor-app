9b008d15afd19950c7ce4c681216cb5c4ffafd15
Patric Poba <poba.dev@outlook.com>
2021-03-13 12:52:30 +0100

    create a partial for google-analytics tracking code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,17 +18,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-   
-    @if( ($analytics ?? true) == true && config('app.env') == 'production')
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168988694-1"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-168988694-1');
-        </script> 
-    @endif
+    
+    @include('layouts.analytics-tracking-code')
 </head>
 <body>
     <div id="app">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,16 +63,7 @@
             }
         </style>
 
-        @if( config('app.env') == 'production')
-            <!-- Global site tag (gtag.js) - Google Analytics -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168988694-1"></script>
-            <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'UA-168988694-1');
-            </script> 
-        @endif
+        @include('layouts.analytics-tracking-code')
     </head>
     <body>
         <div class="flex-center position-ref full-height">
