f2c1b276a4f4540e7689e1b12d40b7d26afec9d3
Patric Poba <poba.dev@outlook.com>
2021-03-13 13:08:47 +0100

    add google analytics code

--- resources/views/layouts/analytics-tracking-code.blade.php
+++ resources/views/layouts/analytics-tracking-code.blade.php
@@ -1,10 +1,11 @@
 @if( ($analytics ?? true) == true && config('app.env') == 'production')
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168988694-1"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-K2V0QLNH5F"></script>
     <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'UA-168988694-1');
-    </script> 
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-K2V0QLNH5F');
+    </script>
 @endif
